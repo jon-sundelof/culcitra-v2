5a23cd0ca27e8127b87e63fa11698db6cea3344e
Jon Sundelöf <70135228+jon-sundelof@users.noreply.github.com>
2021-12-16 19:12:05 +0100

    :rocket: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <header className='App-header'>
+          <h1>Culcitra V2</h1>
+          <span>First Commit - Remember this day</span>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;

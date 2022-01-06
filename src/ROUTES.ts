ee919ba02b8ea8dd619b73ce2968f9fa483af11e
Jon Sundelöf <70135228+jon-sundelof@users.noreply.github.com>
2022-01-06 10:32:41 +0100

    CULCITRA-5 Init lobby layout with content

---
+++ src/ROUTES.ts
@@ -0,0 +1,14 @@
+/*
+    Contains all the routes
+*/
+
+/** Route to the lobby */
+export const LOBBY = '/';
+/** Route to the canvas */
+export const CANVAS = '/canvas';
+/** Route to the craft deck */
+export const CRAFTDECK = '/craft-deck';
+/** Route to the how to play */
+export const HOWTOPLAY = '/how-to-play';
+/** Route to the settings */
+export const SETTINGS = '/settings';
--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,4 +1,7 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+import { CANVAS, CRAFTDECK, HOWTOPLAY, SETTINGS } from '../ROUTES';
+import '../styles/lobby.scss';
 
 interface LobbyProps {}
 
@@ -6,9 +9,25 @@ const Lobby: React.FunctionComponent<LobbyProps> = (
   props: LobbyProps
 ): ReactElement => {
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <main className='lobby-container'>
+      <div className='lobby-content'>
+        <div className='game-header'>
+          <h1>culcitra</h1>
+        </div>
+        <div className='buttons-container'>
+          <Link to={CANVAS}>Start Game</Link>
+          <Link to={CRAFTDECK}>Craft Deck</Link>
+          <Link to={HOWTOPLAY}>How to play</Link>
+          <Link to={SETTINGS}>Settings</Link>
+        </div>
+        <div className='player-container'>
+          <div>
+            <h3>"player name"</h3>
+            <div>*Player-content*</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
